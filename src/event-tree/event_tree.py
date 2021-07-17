432949ffa98f368708cc43131b404c1d625f2f8e
Gareth Walley <gareth@walley.me>
2021-07-17 15:41:01 +0100

    First code for testing CI functionality
    Implemented simple function, and test, to determine if the CI workflow will work.

---
+++ src/event-tree/event_tree.py
@@ -0,0 +1,8 @@
+
+class EventTree:
+    """Creates event trees from panda dataframes."""
+    def __init__(self):
+        self.x = 1
+
+    def increment(self, val):
+        return val + 1
---
+++ src/event-tree/test_event_tree.py
@@ -0,0 +1,10 @@
+from event_tree import EventTree
+
+class TestEventTree:
+    """Runs unit tests for the event tree class"""
+
+    def test_increment(self):
+        et = EventTree()
+        x = 1
+        assert et.increment(x) == 2
+        

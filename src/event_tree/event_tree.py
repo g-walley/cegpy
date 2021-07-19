407a7547942caeda768f1545291a0b389ae81364
Gareth Walley <gareth@walley.me>
2021-07-19 21:15:53 +0100

    added check for sampling zero path param

--- src/event_tree/event_tree.py
+++ src/event_tree/event_tree.py
@@ -1,16 +1,60 @@
 from collections import defaultdict
+import pandas as pd
 import numpy as np
 
 def create_edges(edge_information) -> defaultdict(int):
 	# _edge_creation
 	return defaultdict(int)
 
+def check_list_contains_strings(str_list) -> bool:
+	for tup in str_list:
+		if not isinstance(tup, str):
+			return False
+	
+	return True
+
+def check_sampling_zero_paths_param(sampling_zero_paths) -> list[tuple]:
+	"""Check param 'sampling_zero_paths' is in the correct format"""
+	for tup in sampling_zero_paths:
+		if not isinstance(tup, tuple):
+			return None
+		else:
+			if not check_tuple_contains_strings(tup):
+				return None
+	
+	return sampling_zero_paths
+
+def check_tuple_contains_strings(tup) -> bool:
+	"""Check each element of the tuple to ensure it is a string""" 
+	for elem in tup:
+		if not isinstance(elem, str):
+			return False
+	return True
 
 class EventTree(object):
-
-	"""Creates event trees from panda dataframes."""
+	"""Creates event trees from pandas dataframe."""
 	def __init__(self, params) -> None:
-		pass
+		# pandas dataframe passed via parameters 
+		self.dataframe = params.get("dataframe")
+		self.variables = list(self.dataframe.columns)
+		# sampling zeros paths added manually via parameters 
+		self.sampling_zero_paths = self.get_sampling_zero_paths(params)
+
+		# print(self._create_initial_nodes())
+
+	def get_sampling_zero_paths(self, params) -> list[tuple]:
+		sz_paths = params.get('sampling_zero_paths')
+		if sz_paths == None:
+			return None
+		else:
+			sz_paths = check_sampling_zero_paths_param(sz_paths)
+			if sz_paths is None:
+				error_str = "Parameter 'sampling_zero_paths' should be a list of tuples like so:\n \
+				[('edge_1', 'edge_2', 'edge_3'), ('edge_4',), ...]"
+				raise ValueError(error_str)
+			else:
+				return sz_paths
+			
 
 	def _create_initial_nodes(self) -> list[str]:
 		"""Creates list of all nodes: includes root, situations, leaves"""
@@ -40,4 +84,5 @@ class EventTree(object):
 
 
 if __name__ == "__main__":
-	pass
+	df = pd.read_excel('../data/CHDS.latentexample1.xlsx')
+	et = EventTree({'dataframe' : df})
--- src/event_tree/test_event_tree.py
+++ src/event_tree/test_event_tree.py
@@ -1,5 +1,32 @@
-# from event_tree import EventTree
-# import event_tree as et
+#from event_tree import EventTree
+import event_tree as et
+
+def test_check_list_contains_strings() -> None:
+    str_list = ['blah', 'path/to/node', '123', '~$%^']
+    non_str_list = [123, 'path/to/node', True]
+
+    assert et.check_list_contains_strings(str_list) == True
+    assert et.check_list_contains_strings(non_str_list) == False
+
+def test_check_sampling_zero_paths_param() -> None:
+    """Tests the function that is checking the sampling zero paths param"""
+    sz_paths = [('edge_2',),('edge_1', 'edge_3')]
+    assert et.check_sampling_zero_paths_param(sz_paths) == sz_paths
+    
+    sz_paths = [1,2,3,4]
+    assert et.check_sampling_zero_paths_param(sz_paths) == None
+
+    sz_paths = [('path', 'to'), (123, 'something'), 'path/to']
+    assert et.check_sampling_zero_paths_param(sz_paths) == None
+
+def test_check_tuple_contains_strings() -> None:
+    """Tests the function that is checking that a tuple only contains strings"""
+    tup = ('string thing',)
+    assert et.check_tuple_contains_strings(tup) == True
+    tup = ('string one', 'string two')
+    assert et.check_tuple_contains_strings(tup) == True
+    tup = (1, '2')
+    assert et.check_tuple_contains_strings(tup) == False    
 
 def test_create_edges() -> None:
     assert 1 == 1

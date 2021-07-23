1093365e39b602bf08d2be6baec0a8eded463027
Gareth Walley <gareth@walley.me>
2021-07-23 20:39:05 +0100

    Moved examples to base folder to fix relative path issue

--- example.py
+++ example.py
@@ -1,25 +1,22 @@
-import cegpy
 import logging
-from cegpy.trees.event import EventTree
+from src.cegpy.trees.event import EventTree
+from src.cegpy import logger
 from pathlib import Path
 import pandas as pd
 from datetime import datetime
 
-cegpy.logger.setLevel(level=logging.DEBUG)
-cegpy.logger.info("Starting Main Test Program")
 
-
-df_path = Path(__file__).resolve().parent.parent.joinpath(
+logger.setLevel(level=logging.DEBUG)
+logger.info("Starting Main Test Program")
+df_path = Path(__file__).resolve().parent.joinpath(
     'data/medical_dm_modified.xlsx')
-
+logger.info(str(df_path))
 df = pd.read_excel(df_path)
 et = EventTree({'dataframe': df})
 
 now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
-
 fig_path = Path(__file__).resolve().parent.joinpath(
     'out/medical_dm_event_tree__%s' % dt_string)
-
-cegpy.logger.info("parent: %s" % str(fig_path))
+et.create_figure("image")
 et.create_figure(fig_path)

287af8785e4134d365e05d56d0544c04a614ee2c
Gareth Walley <gareth@walley.me>
2022-04-06 09:24:47 +0000

    Example with fix removed.

--- examples/numerical_sampling_zeros.py
+++ examples/numerical_sampling_zeros.py
@@ -2,7 +2,6 @@ import pandas as pd
 from cegpy import StagedTree
 
 data = pd.read_csv("data/Asym.csv")
-data = data.astype(str)
 missing_paths = [
     ('0', '1', '1', '1'),
     ('1', '0', '1', '1'),

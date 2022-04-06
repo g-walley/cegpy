2e217c3645642034c7e91c3de48e78a98d3c4323
Gareth Walley <gareth@walley.me>
2022-04-06 10:08:22 +0000

    Added broken case and fixed error string.

--- examples/numerical_sampling_zeros.py
+++ examples/numerical_sampling_zeros.py
@@ -8,3 +8,10 @@ missing_paths = [
     ('0', '1', '0', '1')
 ]
 st = StagedTree(data, sampling_zero_paths=missing_paths)
+
+missing_paths = [
+    (0, 1, 1, 1),
+    (1, 0, 1, 1),
+    (0, 1, 0, 1)
+]
+st = StagedTree(data, sampling_zero_paths=missing_paths)

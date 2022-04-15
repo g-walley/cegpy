9c746946342fbd9960f7420529fc74aa9471dcef
Gareth Walley <gareth@walley.me>
2022-04-15 20:08:25 +0000

    Updated some of the poor code in prior setter

--- src/cegpy/trees/staged.py
+++ src/cegpy/trees/staged.py
@@ -4,7 +4,7 @@ from fractions import Fraction
 from itertools import combinations, chain
 import logging
 from operator import add, sub, itemgetter
-from typing import List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 import networkx as nx
 import scipy.special
 from IPython.display import Image
@@ -104,7 +104,7 @@ class StagedTree(EventTree):
         logger.debug("Starting Staged Tree")
 
     @property
-    def prior(self):
+    def prior(self) -> Dict[Tuple[str, str, str]]:
         """Get edge priors from the graph.
 
         ## Returns:
@@ -113,14 +113,13 @@ class StagedTree(EventTree):
         return nx.get_edge_attributes(self, "prior")
 
     @prior.setter
-    def prior(self, prior):
+    def prior(self, prior: List[List[Union[float, Fraction]]]) -> None:
         offset = 0
         for node_idx, node_priors in enumerate(prior):
-            node_name = "s%d" % (node_idx + offset)
-            while self.succ[node_name] == {}:
+            node_name = f"s{node_idx + offset}"
+            while not self.succ[node_name]:
                 offset += 1
-                node_name = "s%d" % (node_idx + offset)
-            pass
+                node_name = f"s{node_idx + offset}"
             for edge_prior_idx, succ_key in enumerate(self.succ[node_name].keys()):
                 label = list(self.succ[node_name][succ_key])[0]
                 self.edges[(node_name, succ_key, label)]["prior"] = node_priors[

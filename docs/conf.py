d457f147b413512756d60f29600a91f324550a33
Gareth Walley <gareth@walley.me>
2022-09-02 11:25:38 +0000

    Moved sphinx docs from docs/source to docs/

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Cegpy"
 copyright = "2022, Gareth Walley, Aditi Shenvi"
 author = "Gareth Walley, Aditi Shenvi"
-release = "1.0"
+release = "1.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
